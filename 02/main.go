777dcd9b7bd1a8a5f1f98a59c5d583fe15a740ff
isLishude <lishude@aliyun.com>
2018-10-27 11:23:32 +0800

    feat: add thread safe map

---
+++ 02/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"sync"
+)
+
+func main() {
+
+}
+
+// SyncMap is thread safe map struct
+type SyncMap struct {
+	rw   *sync.RWMutex
+	data map[interface{}]interface{}
+}
+
+// NewSyncMap it to
+func NewSyncMap() *SyncMap {
+	return &SyncMap{
+		rw:   new(sync.RWMutex),
+		data: make(map[interface{}]interface{}),
+	}
+}
+
+// Get is get value from map
+func (m *SyncMap) Get(key interface{}) (v interface{}, ok bool) {
+	m.rw.RLock()
+	defer m.rw.RUnlock()
+
+	v, ok = m.data[key]
+	return
+}
+
+// Set is set value to map[key]
+func (m *SyncMap) Set(key interface{}, value interface{}) {
+	m.rw.Lock()
+	defer m.rw.Unlock()
+
+	m.data[key] = value
+}
+
+// Delete is to delete map[key]
+func (m *SyncMap) Delete(key interface{}) {
+	m.rw.Lock()
+	defer m.rw.Unlock()
+
+	delete(m.data, key)
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## go-interview
 
-- [00](./00/main.go)
-- [01](./01/main.go)
+- [00 - what will print](./00/main.go)
+- [01 - What will print](./01/main.go)
+- [02 - Thread safe map](./02/main.go)
